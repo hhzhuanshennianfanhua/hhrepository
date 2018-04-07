1d9f08c79f93594a64a4e0ae25e15a5e17b5b531
zzh <zhangzihengheng@163.com>
2018-04-07 11:41:16 +0800

    11

---
+++ src/main/java/com/taotao/manager/service/ItemCatService.java
@@ -0,0 +1,9 @@
+package com.taotao.manager.service;
+
+import com.taotao.manager.pojo.ItemCat;
+
+import java.util.List;
+
+public interface ItemCatService extends BaseService<ItemCat>{
+//    List<ItemCat> queryItemCatByPage(Integer page,Integer rows);
+}
